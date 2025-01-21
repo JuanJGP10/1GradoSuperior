bdc3ba013fb0b4ab74de176d6558fc9e52160bae
Juan José García Phillips <juanilloyt10@gmail.com>
2025-01-21 20:04:02 +0100

    Hasta el ejercicio Producto

---
+++ src/main/java/programacion/tema8/Factura.java
@@ -0,0 +1,20 @@
+package programacion.tema8;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+
+public class Factura {
+
+    private int identificador;
+    private LocalDateTime fechaFactura;
+    private int numeroCliente;
+    private static final double iva = 0.21;
+    // private ArrayList<LineaFactura> lineaFactura = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
