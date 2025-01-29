0872212f006e489618b218a621180ee2921cd2ff
Juan José García Phillips <juanilloyt10@gmail.com>
2025-01-30 00:06:04 +0100

    Se me olvido comentar una cosa de tienda

--- Programacion/src/main/java/programacion/tema8/LaTiendaDelBarrio/Articulo.java
+++ Programacion/src/main/java/programacion/tema8/LaTiendaDelBarrio/Articulo.java
@@ -1,5 +1,34 @@
 package programacion.tema8.LaTiendaDelBarrio;
 
+// En La Tienda del Barrio S.A. necesitan un programa en lenguaje Java que les ayude a
+// gestionar la información sobre los artículos que tienen en la tienda, así como realizar
+// ventas a clientes y compras a proveedores.
+
+// Clase ‘Articulo’
+// • Un artículo tiene un identificador (número entero), un nombre, un precio de
+// venta a cliente, un precio de compra a proveedor, un IVA (%) y un stock
+// (representa la cantidad de ese artículo disponible en tienda). El nombre y el
+// identificador de un artículo no pueden cambiar. El identificador ha de establecerse
+// automáticamente y ser único, podemos llevar un contador de los objetos creados y
+// así el identificador será único. El IVA es el mismo para todos los artículos (un 21%).
+// Tanto el precio de compra como el precio de venta han de ser valores superiores a
+// cero. El precio de venta ha de ser superior al precio de compra. El stock ha de ser
+// superior o igual a cero.
+// • Deberá tener un único constructor así como todos los getters y setters que sea
+// posible.
+// • Deberá tener al menos dos métodos públicos, uno para vender (a cliente) y otro
+// para comprar (a proveedor). En ambos casos deberá pasarse un único argumento
+// con la cantidad de unidades a vender o comprar, y se devolverá un valor indicando
+// si fue posible realizar la operación o no. Si la operación se puede realizar, se
+// deberá modificar el stock del artículo.
+// • Deberá tener un método público que devuelva una cadena de texto de una sola
+// línea con la información sobre dicho artículo: id, nombre, precios, IVA y stock.
+// • En los casos en los que sea necesario, los métodos públicos mostrarán un
+// mensaje por System.err si no es posible realizar la operación solicitada.
+// • Pueden implementarse otros métodos si se considera necesario.
+// • Es obligatorio que esta clase no realice ningún tipo de entrada por teclado ni
+// salida por pantalla (Excepto los mencionados mensajes de error)
+
 /**
  * Clase que representa un artículo en la tienda.
  * Contiene información como el identificador, nombre, precios, stock y
