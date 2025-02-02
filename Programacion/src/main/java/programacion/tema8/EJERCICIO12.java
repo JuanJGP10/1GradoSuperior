d9b4c9b4c028b57179cd071ed2646670bdfc14db
Juan José García Phillips <juanilloyt10@gmail.com>
2025-02-02 01:05:56 +0100

    Hasta el 11, me falta el busca minas

---
+++ Programacion/src/main/java/programacion/tema8/EJERCICIO12.java
@@ -0,0 +1,12 @@
+package programacion.tema8;
+
+public class EJERCICIO12 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] crearTablero(int filas, int columnas, int minas) {
+        int[][] matriz = new int[filas][columnas];
+        return matriz;
+    }
+}
