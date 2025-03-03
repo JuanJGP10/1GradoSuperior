89d4cce207c91a29e61bd7c49be325845a3abbf7
JuanJGP8 <jjgphillips1@gmail.com>
2025-03-03 12:48:59 +0100

    excepciones

---
+++ Programacion/src/main/java/programacion/tema10/Ejercicios/Ejercicio3.java
@@ -0,0 +1,14 @@
+package programacion.tema10.Ejercicios;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        /**
+         * 3. Implementa un programa que cree un vector tipo double de tamaño 5 y luego,
+         * utilizando un bucle, pida cinco valores por teclado y los introduzca en el
+         * vector.
+         * Tendrás que manejar la/las posibles excepciones y seguir pidiendo valores
+         * hasta
+         * rellenar completamente el vector.
+         */
+    }
+}
