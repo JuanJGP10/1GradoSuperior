502c38d4df5f63e3f14c4d2eb1a9de10ae19952a
JuanJGP10 <jjgphillips10119@gmail.com>
2025-03-11 22:41:29 +0100

    hasta el 10

--- Programacion/src/main/java/programacion/tema11/Ejercicios/ejercicio8/Main.java
+++ Programacion/src/main/java/programacion/tema11/Ejercicios/ejercicio8/Main.java
@@ -1,6 +1,6 @@
 package programacion.tema11.Ejercicios.ejercicio8;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
@@ -11,18 +11,31 @@ public class Main {
         System.out.println("Que año quieres ver las temperaturas");
         Scanner scanner = new Scanner(System.in);
         int año = scanner.nextInt();
+
         if ((año % 4 == 0 && año % 100 != 0) || (año % 100 == 0 && año % 400 == 0)) {
             temp = new int[366];
-            Arrays.fill(temp, ThreadLocalRandom.current().nextInt(10, 31));
         } else {
             temp = new int[365];
-            Arrays.fill(temp, ThreadLocalRandom.current().nextInt(10, 31));
         }
 
-        HashMap<Integer, Integer> tempDias = new HashMap<>();
-        for (int i = 10; i < 31; i++) {
-            tempDias.put(i, 1);
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = ThreadLocalRandom.current().nextInt(10, 31);
         }
+
+        HashMap<Integer, ArrayList<Integer>> tempDias = new HashMap<>();
+        for (int i = 10; i < 31; i++) {
+            tempDias.put(i, new ArrayList<>());
+            for (int j = 0; j < temp.length; j++) {
+                if (i == temp[j])
+                    tempDias.get(i).add(j + 1);
+            }
+        }
+
+        for (Integer temperatura : tempDias.keySet()) {
+            System.out.println("Temperatura: " + temperatura + ",  Distribucion: " + tempDias.get(temperatura));
+        }
+
+        scanner.close();
     }
 
 }
---
+++ Programacion/src/main/java/programacion/tema11/Ejercicios/ejercicio9/Main.java
@@ -0,0 +1,32 @@
+package programacion.tema11.Ejercicios.ejercicio9;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] temp;
+        temp = new int[365];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = ThreadLocalRandom.current().nextInt(10, 31);
+        }
+
+        TreeMap<Integer, ArrayList<Integer>> tempDias = new TreeMap<>();
+        for (int i = 10; i < 31; i++) {
+            tempDias.put(i, new ArrayList<>());
+            for (int j = 0; j < temp.length; j++) {
+                if (i == temp[j])
+                    tempDias.get(i).add(j + 1);
+            }
+        }
+
+        System.out.println(
+                "Temperatura maxima: " + tempDias.firstKey() + ",  Numero de apariciones: "
+                        + tempDias.get(tempDias.firstKey()).size());
+        System.out.println(
+                "Temperatura maxima: " + tempDias.lastKey() + ",  Numero de apariciones: "
+                        + tempDias.get(tempDias.lastKey()).size());
+    }
+
+}
