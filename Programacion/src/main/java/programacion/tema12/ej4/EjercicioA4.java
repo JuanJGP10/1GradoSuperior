cb395e948b5ffeafe909a7150aba642b43a88e89
Juan José García Phillips <juanilloyt10@gmail.com>
2025-03-30 21:45:05 +0200

    adada

--- Programacion/src/main/java/programacion/tema12/ej4/EjercicioA4.java
+++ Programacion/src/main/java/programacion/tema12/ej4/EjercicioA4.java
@@ -4,8 +4,33 @@ import java.io.File;
 
 public class EjercicioA4 {
     public static void main(String[] args) {
-        File docs = new File(
-                "C:/Users/juani/Desktop/Clase/1GradoSuperior/Programacion/src/main/java/programacion/tema12/ej4");
-        docs.mkdir();
+        File misCosas = new File(
+                "C:/Users/juani/Desktop/Clase/1GradoSuperior/Programacion/src/main/java/programacion/tema12/ej4/Documentos/Mis_cosas");
+
+        File alfabeto = new File(
+                "C:/Users/juani/Desktop/Clase/1GradoSuperior/Programacion/src/main/java/programacion/tema12/ej4/Documentos/Alfabeto");
+
+        boolean resultado1 = misCosas.mkdir();
+        System.out.println("¿Se ha creado la carpeta 'Documentos/Mis Cosas'? " + resultado1);
+        boolean resultado2 = alfabeto.mkdir();
+        System.out.println("¿Se ha creado la carpeta 'Documentos/Mis Cosas'? " + resultado2);
+
+        File fotOrigen = new File(
+                "C:/Users/juani/Desktop/Clase/1GradoSuperior/Programacion/src/main/java/programacion/tema12/ej4/Documentos/Fotografias");
+        File fotDestino = new File(
+                "C:/Users/juani/Desktop/Clase/1GradoSuperior/Programacion/src/main/java/programacion/tema12/ej4/Documentos/Mis_cosas");
+
+        File libOrigen = new File(
+                "C:/Users/juani/Desktop/Clase/1GradoSuperior/Programacion/src/main/java/programacion/tema12/ej4/Documentos/Libros");
+        File libDestino = new File(
+                "C:/Users/juani/Desktop/Clase/1GradoSuperior/Programacion/src/main/java/programacion/tema12/ej4/Documentos/Mis_cosas");
+
+        fotOrigen.renameTo(fotDestino);
+        libOrigen.renameTo(libDestino);
+
+        for (int i = 65; i <= 90; i++) {
+            File nuevaCarpeta = new File(alfabeto.getParent() + "/" + alfabeto.getName() + "/" + (char) i);
+            nuevaCarpeta.mkdir();
+        }
     }
 }
